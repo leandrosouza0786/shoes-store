91a2432b13800ced30ca9df93d220d0e2ca5bd29
rafashiga <rafa.shiga@gmail.com>
2020-06-01 15:39:30 -0300

    feat(navbar): initial configuration added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CoreRoutingModule } from './core-routing.module';
 
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [HeaderComponent, NavbarComponent],
+  imports: [CommonModule, CoreRoutingModule, RouterModule],
+  exports: [HeaderComponent, NavbarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
