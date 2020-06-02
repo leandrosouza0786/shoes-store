5fde0563d53d2aae9b6dc10329c9424fbab22807
rafashiga <rafa.shiga@gmail.com>
2020-06-02 11:43:03 -0300

    feat(footer): initial configuration added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,11 @@ import { CoreRoutingModule } from './core-routing.module';
 
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent],
+  declarations: [HeaderComponent, NavbarComponent, FooterComponent],
   imports: [CommonModule, CoreRoutingModule, RouterModule],
-  exports: [HeaderComponent, NavbarComponent],
+  exports: [HeaderComponent, NavbarComponent, FooterComponent],
 })
 export class CoreModule {}
