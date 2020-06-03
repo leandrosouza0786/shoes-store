d48452afcd84ba53f92c6d094ab13708f80188f4
rafashiga <rafa.shiga@gmail.com>
2020-06-03 16:53:09 -0300

    feat(footer): create footer bottom component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,9 +6,10 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FooterBottomComponent } from './footer/footer-bottom/footer-bottom.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent, FooterComponent],
+  declarations: [HeaderComponent, NavbarComponent, FooterComponent, FooterBottomComponent],
   imports: [CommonModule, CoreRoutingModule, RouterModule],
   exports: [HeaderComponent, NavbarComponent, FooterComponent],
 })
